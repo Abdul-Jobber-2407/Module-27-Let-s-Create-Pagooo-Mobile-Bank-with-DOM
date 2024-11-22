f8e9994e5da3b20e4eeb97ce8973d251ab4f1402
Abdul-Jobber-2407 <arjobbar2019@gmail.com>
2024-11-22 22:07:40 +0600

    added to a final push

--- js/home.js
+++ js/home.js
@@ -2,7 +2,10 @@
 /**
  s-1: add event handler
  prevent page reload after form submit
- s-2:get money to be added to the account balance
+
+ s-2:get money to be added to the account balance get the pin number
+
+ s-3: verify the pin number
 
  */
 
@@ -17,5 +20,30 @@ document.getElementById('btn-add-money').addEventListener('click',function(event
 
     //get the pin number provided
     const pinNumberInput = document.getElementById('input-pin-number').value ;
-    console.log(pinNumberInput);
+    console.log(typeof pinNumberInput);
+
+    //step-3: Verify pin Number
+    // wrong way to validate pin number 
+    if(pinNumberInput==='1234'){
+        console.log('adding money to your account');
+
+        //step-4: get the current balance
+        const balance = document.getElementById('account-balance').innerText;
+        console.log( typeof balance)
+
+        // step-5: add addMoneyInput with balance
+
+        // const newBalance = balance + addMoneyInput;
+        // console.log(newBalance);
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber ;
+        console.log(newBalance);
+
+        // step - 6: update the balance in the UI / DOM 
+         document.getElementById('account-balance').innerText = newBalance;
+    }
+    else{
+        alert('Failed to add money ! Please try again')
+    }
 })
